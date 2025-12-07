eab43dcfa5e525ccfd6a8b351a65f2574a8df167
“mxylumos” <maoxinyue1216@gmail.com>
2025-12-07 16:12:18 -0500

    vanilla js publish

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@ const addNumbers = (first, second) => {
   return first + second;
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.querySelector("#count-btn");
-  const display = document.querySelector("#count-display");
+const btn = document.querySelector("#count-btn");
+const display = document.querySelector("#count-display");
 
-  let count = 0;
+let count = 0;
 
+if (btn && display) {
   btn.addEventListener("click", () => {
     count = addNumbers(count, 1);
     display.textContent = count;
   });
-});
+}
 
 if (typeof module !== "undefined") {
   module.exports = { addNumbers };
